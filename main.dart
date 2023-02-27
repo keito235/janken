4a9d854bb0e24f4e7a578952c0ae6305c21de907
keito235 <82130615+keito235@users.noreply.github.com>
2023-02-27 13:26:43 +0900

    initial commit
    コンピューターとじゃんけんをして、勝敗を判定してくれるアプリ

---
+++ main.dart
@@ -0,0 +1,144 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: JankenPage(),
+    );
+  }
+}
+
+class JankenPage extends StatefulWidget {
+  const JankenPage({super.key});
+
+  @override
+  State<JankenPage> createState() => _JankenPageState();
+}
+
+class _JankenPageState extends State<JankenPage> {
+  String computerHand = '✊';
+  String myHand = '✊';
+  String result = '引き分け';
+
+  void selectHand(String selectedHand) {
+    myHand = selectedHand;
+    generateComputerHand();
+    judge();
+    setState(() {});
+  }
+
+  void generateComputerHand() {
+    final randomNumber = Random().nextInt(3);
+    computerHand = randomNumberToHand(randomNumber);
+    print(randomNumberToHand(randomNumber));
+  }
+
+  String randomNumberToHand(int randomNumber) {
+    switch (randomNumber) {
+      case 0:
+        return '✊';
+      case 1:
+        return '✌️';
+      case 2:
+        return '✋';
+      default:
+        return '✊';
+    }
+  }
+
+  void judge() {
+    if (computerHand == myHand) {
+      result = '引き分け';
+    } else if (myHand == '✊' && computerHand == '✌️' ||
+        myHand == '✌️' && computerHand == '✋' ||
+        myHand == '✋' && computerHand == '✊') {
+      result = '勝ち';
+    } else {
+      result = '負け';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('じゃんけん'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              result,
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+            SizedBox(
+              height: 64,
+            ),
+            Text(
+              computerHand,
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+            SizedBox(
+              height: 64,
+            ),
+            Text(
+              myHand,
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    selectHand('✊');
+                    // myHand = '✊';
+                    // print('✊');
+                    // setState(() {});
+                  },
+                  child: Text('✊'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    selectHand('✌️');
+                    // myHand = '✌️';
+                    // print('✌️');
+                    // setState(() {});
+                  },
+                  child: Text('✌️'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    selectHand('✋');
+                    // myHand = '✋';
+                    // print('✋');
+                    // setState(() {});
+                  },
+                  child: Text('✋'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
